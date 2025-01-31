1c2bc10ab95cdda27a16d825390e89f55fd754d9
lee-sihun <lee.sihun040223@gmail.com>
2025-01-31 20:58:57 +0900

    feat: not-found 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  pageExtensions: [
+    "page.tsx",
+    "page.ts",
+  ],
   experimental: {
     turbo: {
       rules: {
