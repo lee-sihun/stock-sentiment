e0920db0337b3f7556afc276545bc0c683954aa3
lee-sihun <lee.sihun040223@gmail.com>
2025-01-31 21:58:06 +0900

    fix: webpack 설정 수정

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,36 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-  ],
+  webpack: (config) => {
+    // @ts-expect-error 타입 에러
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, 
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              typescript: true,
+              ext: 'tsx',
+            },
+          },
+        ],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
   experimental: {
     turbo: {
       rules: {
