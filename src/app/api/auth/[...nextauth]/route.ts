153254dc8f846ee3701d4846e31d64b0ba16bed5
lee-sihun <lee.sihun040223@gmail.com>
2025-02-06 19:58:34 +0900

    feat: next-auth 세션 콜백 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,11 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
     }),
   ],
+  callbacks: {
+    session: async ({ session }) => {
+      return session;
+    }
+  }
 });
 
 export { handler as GET, handler as POST };
